aa705d0d85052fb289bb20b25b5ad4cec2ad355a
Will Carty <cartycrabber@gmail.com>
2016-12-19 10:56:26 -0500

    Created Node Created a Node object with two generics, one for the data type and one for the "link" leading to that node. The idea is that the data type will be the Board and the link will be the move leading to that board

---
+++ src/tree/Node.java
@@ -0,0 +1,70 @@
+package tree;
+
+/**
+ * A node for building a tree that stores the node data and the link leading to that data
+ * @author carty
+ *
+ * @param <T> The type of the node data
+ * @param <U> The type of the link leading to the node
+ */
+public class Node<T, U> {
+
+	private T data;
+	private U link;
+	
+	private Node<T, U> parent;
+	
+	public Node(T data, U link, Node<T, U> parent) {
+		this.data = data;
+		this.link = link;
+		this.parent = parent;
+	}
+	
+	public Node(T data) {
+		this(data, null, null);
+	}
+	
+	public int depth() {
+		int d = 0;
+		Node<T, U> node = getParent();
+		while(node != null) {
+			d++;
+			node = node.getParent();
+		}
+		
+		return d;
+	}
+	
+	//Basic Getters and Setters
+	public void setData(T data) {
+		this.data = data;
+	}
+	
+	public T getData() {
+		return data;
+	}
+	
+	public void setLink(U link) {
+		this.link = link;
+	}
+	
+	public U getLink() {
+		return link;
+	}
+	
+	public void setParent(Node<T, U> parent) {
+		this.parent = parent;
+	}
+	
+	public Node<T, U> getParent() {
+		return parent;
+	}
+	
+	//Testing
+	public static void main(String[] args) {
+		Node<Integer, String> tree = new Node<Integer, String>(1);
+		Node<Integer, String> child = new Node<Integer, String>(5, "First", tree);
+		System.out.println(tree.depth());
+		System.out.println(child.depth());
+	}
+}
